4ffd3c85648147972182172f536420c6fb27ad85
TING WEI LUN <wei-lun_ting@asus.com>
2019-02-27 17:07:34 +0800

    Add 1.1 Determine if a string has all unique characters

---
+++ Chap1_Arrays&Strings/1.1Determine if a string has all unique characters/IsUnique.js
@@ -0,0 +1,32 @@
+/**
+ * @param {string} str
+ */
+function isUnique(str) {
+    // Assume it's a ASCII string
+    // We first create a map to record the chars in this array
+    let m = new Array(128).fill(0);
+    for (let i = 0; i < str.length; i++) {
+      m[str.charCodeAt(i)] += 1;
+      if (m[str.charCodeAt(i)] > 1) {
+        return false;
+      }
+    }
+    return true;
+  }
+  
+  /**
+   * @param {string} str
+   */
+  function isU(str) {
+    // Use bits to check uniqueness
+    // GOTCHA: Only valid for 32 bits because bit operator can only manipulate 32 bits
+    let checker = 0;
+    for (let i = 0; i < str.length; i++) {
+      let val = str.charCodeAt(i) - 'a'.charCodeAt(0);
+      if ((checker & (1 << val)) > 0) {
+        return false;
+      }
+      checker |= (1 << val);
+    }
+    return true;
+  }
