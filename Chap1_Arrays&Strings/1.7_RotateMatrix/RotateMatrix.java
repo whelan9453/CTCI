6c31a05aab47ea742a6606d7e41a08cb1f2017b1
whelan <sitdtest@gmail.com>
2019-03-04 21:28:07 +0800

    Add in place edition of 1.6 for Java.

--- Chap1_Arrays&Strings/1.7_RotateMatrix/RotateMatrix.java
+++ Chap1_Arrays&Strings/1.7_RotateMatrix/RotateMatrix.java
@@ -2,9 +2,9 @@ import java.util.Arrays;
 
 class Main {
     public static void main(String[] args) {
-        System.out.println("test rotateMatrixRight");
+        // System.out.println("test rotateMatrixRight");
         int[][] m = new int[][] { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
-        System.out.println(Arrays.deepToString(m));
+        // System.out.println(Arrays.deepToString(m));
         int[][] m2 = rotateMatrixRight(m);
         System.out.println(Arrays.deepToString(m2));
         int[][] m3 = rotateMatrixRight(m2);
@@ -15,7 +15,7 @@ class Main {
         System.out.println(Arrays.deepToString(m5));
         System.out.println("test rotateMatrixLeft");
         m = new int[][] { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
-        System.out.println(Arrays.deepToString(m));
+        // System.out.println(Arrays.deepToString(m));
         m2 = rotateMatrixLeft(m);
         System.out.println(Arrays.deepToString(m2));
         m3 = rotateMatrixLeft(m2);
@@ -24,6 +24,16 @@ class Main {
         System.out.println(Arrays.deepToString(m4));
         m5 = rotateMatrixLeft(m4);
         System.out.println(Arrays.deepToString(m5));
+        System.out.println("test rotateMatrixRight in place");
+        for (int i = 0; i < m.length + 1; i++) {
+            rotateMatrixR(m);
+            System.out.println(Arrays.deepToString(m));
+        }
+        System.out.println("test rotateMatrixLeft in place");
+        for (int i = 0; i < m.length + 1; i++) {
+            rotateMatrixL(m);
+            System.out.println(Arrays.deepToString(m));
+        }
     }
 
     // time:O(n^2), n is the matrix side length
@@ -59,5 +69,51 @@ class Main {
             }
         }
         return ret;
+    }
+
+    // in place rotation clockwise (flip and swap)
+    // time:O(n^2), n is the matrix length
+    // space:O(1), in place replacement
+    static void rotateMatrixR(int[][] m) {
+        int n = m.length;
+        // flip left to right
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n / 2; j++) {
+                int temp = m[i][j];
+                m[i][j] = m[i][n - j - 1];
+                m[i][n - j - 1] = temp;
+            }
+        }
+        // swap diagnosis
+        for (int i = 0; i < n; i++) {
+            for (int c = 0; c < n - i - 1; c++) {
+                int temp = m[i][c];
+                m[i][c] = m[n - c - 1][n - i - 1];
+                m[n - c - 1][n - i - 1] = temp;
+            }
+        }
+    }
+
+    // in place rotation counter-clockwise (flip and swap)
+    // time:O(n^2), n is the matrix length
+    // space:O(1), in place replacement
+    static void rotateMatrixL(int[][] m) {
+        int n = m.length;
+        // flip upside down
+        for (int j = 0; j < n; j++) {
+            for (int i = 0; i < n / 2; i++) {
+                int temp = m[i][j];
+                m[i][j] = m[n - i - 1][j];
+                m[n - i - 1][j] = temp;
+            }
+        }
+        // swap diagnosis
+        for (int i = 0; i < n; i++) {
+            for (int c = 0; c < n - i - 1; c++) {
+                int temp = m[i][c];
+                m[i][c] = m[n - c - 1][n - i - 1];
+                m[n - c - 1][n - i - 1] = temp;
+            }
+        }
     }
 }
