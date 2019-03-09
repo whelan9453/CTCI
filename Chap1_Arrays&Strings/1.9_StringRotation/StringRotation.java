f740d12b2b321305c0ac68388f5088b977f795d2
whelan <sitdtest@gmail.com>
2019-03-09 17:53:58 +0800

    Add a smart solution for 1.9 StringRotation.

--- Chap1_Arrays&Strings/1.9_StringRotation/StringRotation.java
+++ Chap1_Arrays&Strings/1.9_StringRotation/StringRotation.java
@@ -1,16 +1,21 @@
 class Main {
     public static void main(String[] args) {
+        // part1
         System.out.println(isRotation("abc", "abc") == true);
         System.out.println(isRotation("abc", "cab") == true);
         System.out.println(isRotation("waterbottle", "bottlewater") == true);
         System.out.println(isRotation("abc", "abcd") == false);
         System.out.println(isRotation("abc", "bca") == true);
         System.out.println(isRotation("abcd", "bdca") == false);
+        // part2
+        System.out.println(isRotationSmart("abc", "abcd") == false);
+        System.out.println(isRotationSmart("abc", "bca") == true);
+        System.out.println(isRotationSmart("abcd", "bdca") == false);
     }
 
-    // time:O(n^2), n is the string length, we loop n times and each time we need time O(n) to copy the string of length n
+    // time:O(n^2), n is the string length, we loop n times and each time we need
+    // time O(n) to copy the string of length n
     // space:O(n), if we count the temp string with length n in the for loop
-    // brute force to get all rotations of string 1 and compare with string 2
     static boolean isRotation(String str1, String str2) {
         if (str1.length() != str2.length()) {
             return false;
@@ -25,5 +30,15 @@ class Main {
             }
         }
         return false;
+    }
+
+    // this is a tricky one
+    // time:O(2n) = O(n), n is the length of str1
+    // space:O(n), we need n more space for a 2n length str1+str1
+    // if str1 is x+y, and then str2 is y+x
+    // and str2 must be substring of str1+str1 because y+x must be a substring of
+    // x+y+x+y
+    static boolean isRotationSmart(String str1, String str2) {
+        return (str1 + str1).contains(str2);
     }
 }
